ea28c9e0ed9596e125286fe10a694689c2e177ff
Coffee Code <viethoang.nghiem@gmail.com>
2017-03-23 14:04:07 +0700

    fix use bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,40 +1,38 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 import { Router, RouterModule } from '@angular/router';
-import { MaterialModule } from '@angular/material';
-import { NgProgressModule } from 'ng2-progressbar';
-import 'hammerjs';
-import { LocationStrategy, HashLocationStrategy } from "@angular/common";
-
 import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material';
+import { MenuComponent } from './menu/menu.component';
 import { MusicComponent } from './music/music.component';
 import { MusicDetailComponent } from './music-detail/music-detail.component';
-import { NotfoundComponent } from './notfound/notfound.component';
+import { MusicService } from './music.service';
+import { NgProgressModule } from 'ng2-progressbar';
 import { PlayerComponent } from './player/player.component';
 import { SearchComponent } from './search/search.component';
-import { SearchFormComponent } from './search-form/search-form.component';
-import { MenuComponent } from './menu/menu.component';
 import { MenuMobileComponent } from './menu-mobile/menu-mobile.component';
+import { SearchFormComponent } from './search-form/search-form.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 import { ListSongComponent } from './list-song/list-song.component';
 import { ListHotSongComponent } from './list-hot-song/list-hot-song.component';
-
-import { MusicService } from './music.service';
+import 'hammerjs';
+import { LocationStrategy, HashLocationStrategy } from "@angular/common";
 
 @NgModule({
   declarations: [
     AppComponent,
+    MenuComponent,
     MusicComponent,
     MusicDetailComponent,
-    NotfoundComponent,
     PlayerComponent,
     SearchComponent,
-    SearchFormComponent,
-    MenuComponent,
     MenuMobileComponent,
-    ListSongComponent,
-    ListHotSongComponent
+    ListHotSongComponent,
+    SearchFormComponent,
+    NotfoundComponent,
+    ListSongComponent
   ],
   imports: [
     MaterialModule,
